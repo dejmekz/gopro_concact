9fff3bf71fdf8f31fb3bfa9e7727c92ca5004bc9
dejmekz <zdenek.dejmek@ddash.cz>
2025-01-27 10:39:09 +0100

    Clenup code

--- gopro_concat.py
+++ gopro_concat.py
@@ -14,60 +14,16 @@ Concat Steps:
     downconvert.
 '''
 
-import inspect
-import logging
-from os import listdir, chdir
+from os import listdir
 from os.path import isfile, join
 import re
 import subprocess
-import sys
-import time
-
 import get_mediainfo as media
 
 from datetime import datetime
 from pathlib import Path
-from operator import itemgetter
-from pymediainfo import MediaInfo
-from time import localtime, strftime
 
-
-def print_intro():
-    '''
-    Get the user input for starting values (Source, Output, Downcovert)
-    and begin the concat process.
-    '''
-    open_msg = f"\n\
-    ================================================================\n \
-                GoPro Automation Script, version 0.2 \n \
-    This script will take a collection of chaptered GoPro video files \n \
-    and stitch them together with the option to also downconvert them \n \
-    to a 10Mbit file. \n \
-    ================================================================\n"
-
-    print(open_msg)
-
-    source_path = "/mnt/e/JANA_Skoleni/2024_09_01/" #str(input("Source path: "))
-
-    output_path = "/mnt/f/Video/" # str(input("Output path: "))
-
-    down_convert = "No" #str(input("Downconvert the GoPro files to 10Mbit [Yes/No]: "))
-
-    while True:
-        if down_convert.lower() == 'yes':
-            down_convert = True
-            break
-        elif down_convert.lower() == 'no':
-            down_convert = False
-            break
-        else:
-            print(f"{down_convert} is not a valid choice.")
-            down_convert = str(input("Please select Yes or No (Y/N): "))
-            continue
-
-    return [source_path, output_path, down_convert]
-
-def get_gopro_list(source_path):
+def get_grouped_files(source_path: str, debug: bool = False) -> dict:
     '''
     Create a list of all the MP4 files in the given Source Dir.
     File names must follow the specific pattern defined in the
@@ -75,38 +31,35 @@ def get_gopro_list(source_path):
     '''
 
     all_gopro_files = []
-    all_gopro_keys = {}
+    grouped_gopro_files = {}
 
-    onlyfiles = [f for f in listdir(source_path) if isfile(join(source_path, f))]
+    only_files = [f for f in listdir(source_path) if isfile(join(source_path, f))]
 
-    # print("FILE LIST: " + str(file_list))
+    if debug is True:
+        print("FILE LIST: " + str(only_files))
 
-    regex = r"(GH\d{6})\."
+    pat = re.compile(r"[gGhH]{2}\d{6}\.[mMpP]{2}4")
 
-    for gopro_file in onlyfiles:
-        if gopro_file.endswith('.MP4'):           
-            gp = re.search(regex, gopro_file)
-            if gp is not None:
-                all_gopro_files.append(gopro_file)
-        else:
-            continue
+    for file_name in only_files:
+        if pat.match(file_name):
+            all_gopro_files.append(file_name)
 
-    print("GP KEY LIST: " + str(all_gopro_files))
+    if debug is True:
+        print("Valid gopro files: " + str(all_gopro_files))
 
-    for file in all_gopro_files:
-        filenum = file[4:]
-        filekey = file[4:8]
+    for file_name in all_gopro_files:
+        file_number = file_name[4:8]
 
-        if filekey not in all_gopro_keys:
-            fstring = f"(GH\\d{{2}}{filenum})"
+        if file_number not in grouped_gopro_files:
+            fstring = f"[gGhH]\\d{{2}}{file_number}\.[mM][pP]4"
             r = re.compile(fstring)
             gplist = sorted(list(filter(r.match, all_gopro_files)))
-            #gplist.insert(0,file)
-            all_gopro_keys.update({filekey:gplist})
+            grouped_gopro_files.update({file_number:gplist})
 
+    if debug is True:
+        print("Grouped gopro files by part number: " + str(grouped_gopro_files))
 
-    print(all_gopro_keys)
-    return all_gopro_keys
+    return grouped_gopro_files
 
 
 def create_datetime(encoded_date):
@@ -131,7 +84,7 @@ def create_datetime(encoded_date):
     return creation_date_str
 
 
-def create_ffmpeg_txtfiles(gprkey, gopr_dict, source_path, output_path):
+def create_ffmpeg_txtfile(file_number :str, grouped_files :dict, source_path :str, output_path :str, debug :bool = False) -> str:
 
     '''
     create a txt file with paths to a set our MP4 source files.
@@ -140,51 +93,69 @@ def create_ffmpeg_txtfiles(gprkey, gopr_dict, source_path, output_path):
     file '/path/to/file2'
     file '/path/to/file3'
     '''
-    gpr_txt_path = Path(output_path + gprkey + '.txt')
+    output_text_file_path = Path(output_path + file_number + '.txt')
 
-    if gpr_txt_path.exists() is True:
-        pass
-        print("PASS on TXT FILE")
-    else:
-        gpr_sources_txt = open(gpr_txt_path, 'a')
+    if debug is True:
+        print("OUTPUT TEXT FILE PATH: " + str(output_text_file_path))
 
-        # print("GPR TXT PATH: " + str(gpr_txt_path))
+    output_text_file = open(output_text_file_path, 'w')
+    gprfile_list = grouped_files[file_number]
 
-        gprfile_list = gopr_dict[gprkey]
+    for file in gprfile_list:
+        file_stmnt = "file " + '\'' + source_path + file + '\'' + "\n"
+        output_text_file.write(file_stmnt)
 
+    output_text_file.close()
+
+    return output_text_file_path
+
+def create_ffmpeg_txtfile_big(file_numbers :list, grouped_files :dict, source_path :str, output_path :str, debug :bool = False) -> str:
+
+    '''
+    create a txt file with paths to a set our MP4 source files.
+    the text file is passed into the FFMEG statment as the input.
+    file '/path/to/file1'
+    file '/path/to/file2'
+    file '/path/to/file3'
+    '''
+    output_text_file_path = Path(output_path + 'all_in_one.txt')
+
+    if debug is True:
+        print("OUTPUT TEXT FILE PATH: " + str(output_text_file_path))
+
+    output_text_file = open(output_text_file_path, 'w')
+    
+    for file_number in file_numbers:   
+        gprfile_list = grouped_files[file_number]
         for file in gprfile_list:
             file_stmnt = "file " + '\'' + source_path + file + '\'' + "\n"
-            gpr_sources_txt.write(file_stmnt)
+            output_text_file.write(file_stmnt)
 
-            # print("FILE STMNT: " + file_stmnt)
+    output_text_file.close()
 
-        gpr_sources_txt.close()
+    return output_text_file_path
 
-    return gpr_txt_path
-
-
-def ffmpeg_concat(gprkey, gopr_dict, source_path, output_path):
+def ffmpeg_concat_by_file_number(file_number :str, grouped_files :dict, source_path :str, output_path :str, debug :bool = False):
     '''
     Use FFMPEG subprocess call to merge a set of MP4 files.
     '''
-    chdir(output_path)
 
-    gpr_txt_path = create_ffmpeg_txtfiles(gprkey, gopr_dict, source_path, output_path)
+    gpr_txt_path = create_ffmpeg_txtfile(file_number, grouped_files, source_path, output_path, debug)
 
-    firstfile = gopr_dict[gprkey][0]
-
+    firstfile = grouped_files[file_number][0]
     mediainfo = media.get_mediainfo(source_path, firstfile)
 
-    print("MEDIA INFO: " + str(mediainfo))
+    if debug is True:
+        print("MEDIA INFO: " + str(mediainfo))
 
     encoded_date = mediainfo['v_encoded_date']
-
+    creation_time = 'creation_time=' + encoded_date
     gprkey_date = create_datetime(encoded_date)
-    creation_time = 'creation_time=' + encoded_date[4:]
 
-    print("CREATION TIME: " + creation_time)
+    if debug is True:
+        print("CREATION TIME: " + creation_time + " [ " + encoded_date + " ]")
 
-    mp4_output = gprkey + '_' + gprkey_date[:-6] + '.MP4'
+    mp4_output = file_number + '_' + gprkey_date[:-6] + '.MP4'
 
     ffmpeg_cmd = [
                   'ffmpeg', '-safe', '0', '-f', 'concat',  '-i',
@@ -192,27 +163,62 @@ def ffmpeg_concat(gprkey, gopr_dict, source_path, output_path):
                   mp4_output
                   ]
 
-    output_log = open(output_path + '/' + gprkey + '_output.log', 'a')
+    output_log = open(output_path + '/' + file_number + '_output.log', 'a')
 
     sp = subprocess.Popen(ffmpeg_cmd,
                           shell=False,
                           stderr=output_log,
                           stdout=output_log)
 
-    stdout, stderr = sp.communicate(input='N')
+    _, _ = sp.communicate(input='N')
 
     output_log.close()
 
-    return mp4_output, mediainfo, creation_time
+def ffmpeg_concat_as_one_file(file_numbers :list, grouped_files :dict, source_path :str, output_path :str, debug :bool = False):
+    '''
+    Use FFMPEG subprocess call to merge a set of MP4 files.
+    '''
 
+    gpr_txt_path = create_ffmpeg_txtfile_big(file_numbers, grouped_files, source_path, output_path, debug)
+
+    firstfile = grouped_files[file_numbers[0]][0]
+    mediainfo = media.get_mediainfo(source_path, firstfile)
+
+    if debug is True:
+        print("MEDIA INFO: " + str(mediainfo))
+
+    encoded_date = mediainfo['v_encoded_date']
+    creation_time = 'creation_time=' + encoded_date
+    gprkey_date = create_datetime(encoded_date)
+
+    if debug is True:
+        print("CREATION TIME: " + creation_time + " [ " + encoded_date + " ]")
+
+    mp4_output = 'all_in_one_' + gprkey_date[:-6] + '.MP4'
+
+    ffmpeg_cmd = [
+                  'ffmpeg', '-safe', '0', '-f', 'concat',  '-i',
+                  gpr_txt_path, '-c', 'copy', '-metadata', creation_time,
+                  mp4_output
+                  ]
+
+    output_log = open(output_path + '/all_in_one_output.log', 'a')
+
+    sp = subprocess.Popen(ffmpeg_cmd,
+                          shell=False,
+                          stderr=output_log,
+                          stdout=output_log)
+
+    _, _ = sp.communicate(input='N')
+
+    output_log.close()
 
 def ffmpeg_downconvert(gprkey, gopr_dict, source_path, output_path):
     '''
     Use a FFMPEG subprocess call to downconvert a merged MP4 file.
     '''
-    os.chdir(output_path)
 
-    mp4_output, mediainfo, creation_time = ffmpeg_concat(gprkey, gopr_dict, source_path, output_path)
+    mp4_output, mediainfo, creation_time = ffmpeg_concat_by_file_number(gprkey, gopr_dict, source_path, output_path)
 
     bitrate = mediainfo['v_bit_rate']
     bitratemode = mediainfo['v_bit_rate_mode']
@@ -257,5 +263,3 @@ def ffmpeg_downconvert(gprkey, gopr_dict, source_path, output_path):
     print(stderr, stdout)
 
     output_log.close()
-
-    print("COMPLETE")
--- gopro_main.py
+++ gopro_main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 '''
-GoPro-Concat-Automation  v 0.2
-March 2019
+GoPro-Concat-Automation  v 0.5
+January 2025
 created by: steven cucolo stevenc.github@gmail.com
+updated by: Zdeněk Dejmek
 
 GoPro records MP4 files that are segmented based on a 4Gb file size limit.
 This script will go through a large batch of segmented GoPro files and
@@ -12,47 +13,85 @@ An option to downconvert the merged file is also provided. The file merge
 and downconvert is performed by FFMPEG.
 '''
 
+PROG_VERSION = 0.5
+DEBUG_ENABLED = False
+
+from argparse import ArgumentParser
 import gopro_concat as gp
+from os import chdir
+import sys
 
+def ParseInputArguments() -> list:
+    global DEBUG_ENABLED
 
-def gopro_main():
+    #parser = ArgumentParser(prog="Alerta tester",description="Test create objects of Alerta", epilog="Execute automation test of CLI interface")
+    parser = ArgumentParser(description="Test create objects of Alerta", epilog="Execute automation test of CLI interface")
+    parser.add_argument('-s','--source', action='store', help='source input path', default=None, type=str)
+    parser.add_argument('-o','--output', action='store', help='ouput path', default=None, type=str)
+    parser.add_argument('-a','--all', action='store_true', help='concat all files in one file')
+    parser.add_argument('-v','--version', action='version', help='print version information and exit', version="%(prog)s " + PROG_VERSION)
+    parser.add_argument('-d','--debug', action='store_true', help='print alertinguicli information')
+
+    parsed_args=parser.parse_args()
+
+    source = parsed_args.source
+    output = parsed_args.output
+
+    if (source is None) or (output is None):
+        parser.print_help()
+        sys.exit("Missing source path or output path")
+
+    DEBUG_ENABLED = parsed_args.debug
+    all_in_one = parsed_args.all
+
+    #print(parsed_args)
+
+    return [source, output, all_in_one]
+
+def gopro_main(source_path: str, output_path: str, all_in_one: bool):
     '''
     Get the user input and pass it into the concat and/or downconvert
     functions.
     '''
-    gp_vars = gp.print_intro()
 
-    source_path = gp_vars[0]
-    output_path = gp_vars[1]
-    down_convert = gp_vars[2]
+    print(f"\n\
+    ================================================================\n \
+                GoPro Automation Script, version 0.5 \n \
+    This script will take a collection of chaptered GoPro video files \n \
+    and stitch them together with the option to also downconvert them \n \
+    to a 10Mbit file. \n \
+    ================================================================\n"
+    )    
 
-    start_message = f"\
+    print(f"\
     ================================================================\n \
         Starting the GoPro concat with these values : \n \
-        Source Path:   {str(source_path)} \n \
-        Output Path:   {str(output_path)} \n \
-        DownConvert (Y/N):    {str(down_convert)} \n \
-    ===========================================================\n "
+            Source Path: {str(source_path)} \n \
+            Output Path: {str(output_path)} \n \
+             All in one: {str(all_in_one)} \n \
+      DownConvert (Y/N): No \n \
+    ===========================================================\n ")
 
-    print(start_message)
+    grouped_gopro_files = gp.get_grouped_files(source_path, DEBUG_ENABLED)
+    file_numer_list = sorted(list(grouped_gopro_files.keys()))
 
-    gopro_dict = gp.get_gopro_list(source_path)
-    gprkey_list = sorted(list(gopro_dict.keys()))
+    chdir(output_path)
 
-    if down_convert == False:
-
-        for gprkey in gprkey_list:
-            gp.ffmpeg_concat(gprkey, gopro_dict, source_path, output_path)
-            print('Processing concat now...')
-
+    if all_in_one == True:
+        print('Processing concat now...')
+        gp.ffmpeg_concat_as_one_file(file_numer_list, grouped_gopro_files, source_path, output_path, DEBUG_ENABLED)            
     else:
+        print('Processing concat now...')
+        for file_number in file_numer_list:            
+            gp.ffmpeg_concat_by_file_number(file_number, grouped_gopro_files, source_path, output_path, DEBUG_ENABLED)            
 
-        for gprkey in gprkey_list:
-            ffmpeg_downconvert = gp.ffmpeg_downconvert(gprkey, gopro_dict, source_path, output_path)
-
-        print( "Processing downconvert now...")
+    #else:
+    #    print( "Processing downconvert now...")
+    #    for file_number in file_numer_list:
+    #        gp.ffmpeg_downconvert(file_number, grouped_gopro_files, source_path, output_path)
 
     print('FFMPEG process complete.')
 
-gopro_main()
-
+if __name__ == '__main__':
+    args = ParseInputArguments()
+    gopro_main(args[0], args[1], args[2])
